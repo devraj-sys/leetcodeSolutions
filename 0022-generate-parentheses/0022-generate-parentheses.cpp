5d5b46afbc1c5ec9a03311b0be2af9fc190a1a44
Ramansh Shah <shahramansh@gmail.com>
2026-04-13 17:26:21 +0530

    Time: 0 ms (100%), Space: 13 MB (89.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    void solve(int open, int close, int n, string curr, vector<string>& res) {
+    void solve(int open, int close, int n, string& curr, vector<string>& res) {
         if (curr.length() == 2 * n) {
             res.push_back(curr);
             return;
         }
 
-        // Add '('
         if (open < n) {
-            solve(open + 1, close, n, curr + "(", res);
+            curr.push_back('(');        // DO
+            solve(open + 1, close, n, curr, res);
+            curr.pop_back();            // UNDO
         }
 
-        // Add ')'
         if (close < open) {
-            solve(open, close + 1, n, curr + ")", res);
+            curr.push_back(')');        // DO
+            solve(open, close + 1, n, curr, res);
+            curr.pop_back();            // UNDO
         }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        solve(0, 0, n, "", res);
+        string curr = "";
+        solve(0, 0, n, curr, res);
         return res;
     }
 };
