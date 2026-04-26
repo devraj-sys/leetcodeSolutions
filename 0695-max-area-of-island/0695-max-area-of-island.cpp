aa36428b0f1325a2df128bd44cb5d821a39fd32d
Ramansh Shah <shahramansh@gmail.com>
2026-04-26 23:00:44 +0530

    Time: 5 ms (29.04%), Space: 27.5 MB (77.86%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-    int n = grid.size();
-    int m = grid[0].size();
+    int dfs(vector<vector<int>>& grid, int r, int c ){
+        int m = grid.size();
+        int n = grid[0].size();
 
-    // boundary + water check
-    if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] == 0)
-        return 0;
+        if(r >= m || r < 0 || c >= n || c < 0 || grid[r][c] == 0 ){
+            return 0;
+        }
 
-    // mark visited
-    grid[r][c] = 0;
+        grid[r][c] = 0;
 
-    int area = 1;
+        int area = 1;
 
-    // explore 4 directions
-    area += dfs(grid, r+1, c);
-    area += dfs(grid, r-1, c);
-    area += dfs(grid, r, c+1);
-    area += dfs(grid, r, c-1);
+        area += dfs(grid , r+1 , c  );
+        area += dfs(grid , r , c-1  );
+        area += dfs(grid , r , c+1  );
+        area += dfs(grid , r-1 , c  );
 
-    return area;
-}
+        return area;
+    }   
 
-int maxAreaOfIsland(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
 
-    int maxArea = 0;
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+       int m = grid.size();
+       int n = grid[0].size();
+       int ans = 0;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (grid[i][j] == 1) {
-                int area = dfs(grid, i, j);
-                maxArea = max(maxArea, area);
+       for(int i = 0 ; i < m ; i ++){
+        for(int j = 0 ; j < n ; j++){
+            if(grid[i][j] == 1){
+                int area = dfs(grid , i , j  );
+                ans = max(ans, area);
             }
         }
-    }
+       }
 
-    return maxArea;
-}
+       return ans;
+    }
 };
